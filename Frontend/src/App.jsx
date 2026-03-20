d38339b829a767e73d8d3042887cc882d199d40c
harsha vardhan <harshavardhant2410@gmail.com>
2026-03-20 12:59:37 +0530

    frontend intialised

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './index.css'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return <div classname>
+    <Navbar />
+  <div/>
+    
+}
+
+export default App
